0fb2c042853ddc126a076eac334ad1f167cb3807
wangpenbin <2063870013@qq.com>
2019-11-28 10:44:30 +0800

    first commit

---
+++ scripts/test_hello.py
@@ -0,0 +1,24 @@
+import allure
+import pytest
+
+
+class TestHello:
+
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_hello1(self):
+        assert True
+
+    def test_hello2(self):
+        assert True
+
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_hello3(self):
+        assert False
+
+    def test_hello4(self):
+        assert False
+
+    def test_hello5(self):
+        assert True
+
+
